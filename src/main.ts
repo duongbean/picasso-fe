49b0a1296ac051d5c84abb52da3c55f94c0de6eb
canviet1 <vietgame431@gmail.com>
2025-03-08 00:21:41 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import "@angular/compiler";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
