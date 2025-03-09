8c1889a0e9b9e22c35d922db46e73e006ef7651c
Nguyen Tu Binh <109911046+nguyentubinh23@users.noreply.github.com>
2025-03-09 18:34:41 +0700

    Update main.server.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideServerRendering } from '@angular/platform-server';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-
+import "@angular/compiler";
 // Cấu hình SSR cho Cloudflare Workers
 const bootstrap = () => bootstrapApplication(AppComponent, {
   ...config,
